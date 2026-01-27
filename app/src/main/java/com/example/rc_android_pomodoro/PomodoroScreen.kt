432ddc628b119b0aa141a2d918b6c08b63528834
nadia-nh <nadia.heredia.nh@gmail.com>
2026-01-27 15:44:51 -0500

    Move TimerConfig to the data package

--- app/src/main/java/com/example/rc_android_pomodoro/PomodoroScreen.kt
+++ app/src/main/java/com/example/rc_android_pomodoro/PomodoroScreen.kt
@@ -30,6 +30,7 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.rc_android_pomodoro.viewmodel.PomodoroViewModel
 import com.example.rc_android_pomodoro.viewmodel.TestViewModel
+import com.example.rc_android_pomodoro.data.TimerConfig
 
 @Composable
 fun PomodoroScreen(
@@ -178,19 +179,4 @@ fun PomodoroTimeInputPreview() {
 @Composable
 fun PomodoroButtonPreview() {
     PomodoroButton(isRunning = true, onStart = {}, onStop = {})
-}
-
-private object TimerConfig {
-    // Generate: 1, 2, 3 ... 15
-    private val fastIncrements = (1..15).toList()
-    // Generate: 20, 25, 30 ... 45
-    private val slowIncrements = (20..45 step 5).toList()
-
-    // Final list: [1, 2, 3, ... 15, 20, 25, ... 45]
-    val durations = fastIncrements + slowIncrements
-
-    // Number of steps for the slider, removes start and end values
-    val sliderNumSteps = durations.size - 2
-
-    val maxIndex = (durations.size - 1).toFloat()
 }
---
+++ app/src/main/java/com/example/rc_android_pomodoro/data/TimerConfig.kt
@@ -0,0 +1,16 @@
+package com.example.rc_android_pomodoro.data
+
+object TimerConfig {
+    // Generate: 1, 2, 3 ... 15
+    private val fastIncrements = (1..15).toList()
+    // Generate: 20, 25, 30 ... 45
+    private val slowIncrements = (20..45 step 5).toList()
+
+    // Final list: [1, 2, 3, ... 15, 20, 25, ... 45]
+    val durations = fastIncrements + slowIncrements
+
+    // Number of steps for the slider, removes start and end values
+    val sliderNumSteps = durations.size - 2
+
+    val maxIndex = (durations.size - 1).toFloat()
+}
