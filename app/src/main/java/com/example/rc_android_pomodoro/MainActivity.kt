39a77d259665861e9a7df73b59531d45ee5ab559
nadia-nh <nadia.heredia.nh@gmail.com>
2026-01-27 14:21:17 -0500

    Create PomodoroMain to switch between PomodoroScreen and HistoryScreen

--- app/src/main/java/com/example/rc_android_pomodoro/MainActivity.kt
+++ app/src/main/java/com/example/rc_android_pomodoro/MainActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.rc_android_pomodoro.ui.theme.RCandroidpomodoroTheme
@@ -18,19 +14,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RCandroidpomodoroTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    val app = LocalContext.current.applicationContext as PomodoroApplication
-                    val dao = app.database.sessionDao()
+                val app = LocalContext.current.applicationContext as PomodoroApplication
+                val dao = app.database.sessionDao()
 
-                    val pomodoroViewModel: PomodoroViewModel = viewModel(
-                        factory = PomodoroViewModelFactory(dao)
-                    )
+                val pomodoroViewModel: PomodoroViewModel = viewModel(
+                    factory = PomodoroViewModelFactory(dao)
+                )
 
-                    PomodoroScreen(
-                        modifier = Modifier.padding(innerPadding),
-                        viewModel = pomodoroViewModel
-                        )
-                }
+                PomodoroMain(viewModel = pomodoroViewModel)
             }
         }
     }
---
+++ app/src/main/java/com/example/rc_android_pomodoro/PomodoroMain.kt
@@ -0,0 +1,108 @@
+package com.example.rc_android_pomodoro
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.filled.Timer
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun PomodoroMain(viewModel: PomodoroViewModel) {
+    val currentScreen = viewModel.currentScreen.collectAsState()
+    PomodoroMainStateless(
+        currentScreen = currentScreen.value,
+        onClickMain = { viewModel.navigateTo(AppScreen.Main) },
+        onClickHistory = { viewModel.navigateTo(AppScreen.History) },
+        mainScreen = { innerPadding ->
+            PomodoroScreen(
+                modifier = Modifier.padding(innerPadding),
+                viewModel = viewModel) },
+        historyScreen = { innerPadding ->
+            HistoryScreen(
+                modifier = Modifier.padding(innerPadding),
+                viewModel = viewModel) }
+        )
+}
+
+@Composable
+fun PomodoroMainStateless(
+    currentScreen: AppScreen,
+    onClickMain: () -> Unit,
+    onClickHistory: () -> Unit,
+    mainScreen: @Composable ((PaddingValues) -> Unit) = {},
+    historyScreen: @Composable ((PaddingValues) -> Unit) = {}
+) {
+    Scaffold(
+        bottomBar = {
+            PomodoroMainBottomBar(
+                currentScreen,
+                onClickMain = onClickMain,
+                onClickHistory = onClickHistory
+            )
+        }
+    ) { innerPadding ->
+        when (currentScreen) {
+            AppScreen.Main -> mainScreen(innerPadding)
+            AppScreen.History -> historyScreen(innerPadding)
+        }
+    }
+}
+
+@Composable
+fun PomodoroMainBottomBar(
+    currentScreen: AppScreen,
+    onClickMain: () -> Unit,
+    onClickHistory: () -> Unit
+) {
+    NavigationBar {
+        NavigationBarItem(
+            selected = currentScreen == AppScreen.Main,
+            onClick = onClickMain,
+            icon = {
+                Icon(Icons.Default.Timer,
+                    "Timer") },
+            label = { Text("Timer") }
+        )
+        NavigationBarItem(
+            selected = currentScreen == AppScreen.History,
+            onClick = onClickHistory,
+            icon = {
+                Icon(
+                    Icons.AutoMirrored.Filled.List,
+                    "History") },
+            label = { Text("History") }
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PomodoroMainPreview() {
+    PomodoroMainStateless(
+        currentScreen = AppScreen.Main,
+        onClickMain = {},
+        onClickHistory = {},
+        mainScreen = {},
+        historyScreen = {}
+    )
+}
+
+
+@Preview
+@Composable
+fun PomodoroMainBottomBarPreview() {
+    PomodoroMainBottomBar(
+        currentScreen = AppScreen.Main,
+        onClickMain = {},
+        onClickHistory = {}
+    )
+}
